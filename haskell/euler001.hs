9fd88363c10abbadb39b9e6c249649b2fd9232b6
Brian Schubert <brianm.schubert@gmail.com>
2018-06-02 17:30:31 -0400

    Replace parentheses with the apply operator

--- haskell/euler001.hs
+++ haskell/euler001.hs
@@ -1,15 +1,15 @@
 target = 1000
 
 divisibleByAny :: Integral a => [a] -> a -> Bool
-divisibleByAny ns num = or (map (\x -> mod num x == 0) ns)
+divisibleByAny ns num = or $ map (\x -> mod num x == 0) ns
 
 multiplesOfAny :: Integral a => [a] -> [a]
 multiplesOfAny ns = [x | x <- [1..], divisibleByAny ns x]
 
-solution cap = sum (takeWhile (\x -> x < cap) (multiplesOfAny [3, 5]))
+solution cap = sum $ takeWhile (\x -> x < cap) $ multiplesOfAny [3, 5]
 
-main = print (solution target)
+main = print $ solution target
 
 -- Or, as a one-liner
--- main = print (sum [x | x <- [1..999], mod x 3 == 0 || mod x 5 == 0])
+-- main = print $ sum [x | x <- [1..999], mod x 3 == 0 || mod x 5 == 0]
     
