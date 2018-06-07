73a64129c5f4dc1edbdf7341d4861347ebc08ca7
Brian Schubert <brianm.schubert@gmail.com>
2018-06-07 14:58:59 -0400

    Move factorial and factoring utils to common module

--- haskell/Common.hs
+++ haskell/Common.hs
@@ -1,4 +1,9 @@
-module Common (digitSum, fibonacci) where
+module Common (factorial, digitSum, fibonacci, factorPairs, factorSet) where
+
+import qualified Data.Set as Set
+
+factorial :: Integral a => a -> a
+factorial n = product [2..n]
 
 digitSum :: Integral a => a -> a
 digitSum 0 = 0
@@ -10,4 +15,18 @@ digitSum n
 
 fibonacci :: Integral a => [a]
 fibonacci = map fst $ iterate (\(a,b) -> (b, b + a)) (0, 1)
+
+factorPairs :: Integral a => a -> [(a,a)]
+factorPairs n = go n 1
+    where top = 1 + (floor . sqrt . fromIntegral) n
+          go n s
+              | s >= top = []
+              | n `mod` s == 0 = (s, div):(go n next)
+              | otherwise = go n next
+              where div = n `quot` s
+                    next = succ s
+
+factorSet :: Integral a => a -> Set.Set a
+factorSet = Set.fromList . flattenPairs . factorPairs
+     where flattenPairs = foldr (\(a, b) acc -> a : b : acc) []
 
--- haskell/Euler012.hs
+++ haskell/Euler012.hs
@@ -1,19 +1,11 @@
 module Euler012 where
 
+import Common (factorPairs)
+
 target = 500
 
 triangleNumbers :: Integral a => [a]
 triangleNumbers = scanl1 (+) [1..]
-
-factorPairs :: Integral a => a -> [(a,a)]
-factorPairs n = go n 1
-    where top = 1 + (floor . sqrt . fromIntegral) n
-          go n s
-              | s >= top = []
-              | n `mod` s == 0 = (s, div):(go n next) 
-              | otherwise = go n next
-              where div = n `quot` s
-                    next = succ s
 
 hasExcessFactors :: Integral a => a -> a -> Bool
 hasExcessFactors min n = min < 2 * (fromIntegral . length . factorPairs) n
