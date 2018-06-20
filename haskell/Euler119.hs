2273bae4511ca81290be7aae250e95435bbb012e
Brian Schubert <brianm.schubert@gmail.com>
2018-06-20 09:36:18 -0400

    Add Haskell solutions for problems 119 and 120

---
+++ haskell/Euler119.hs
@@ -0,0 +1,22 @@
+module Euler119 where
+
+import Common (digitListBase)
+
+target = 30
+
+isDigitPower :: Integral a => a -> Bool
+isDigitPower n
+  | n < 10 || digitSum == 1 = False
+  | otherwise = go 2
+  where
+    digitSum = sum . digitListBase 10 $ n
+    go power = case digitPowerSum `compare` n of
+        EQ -> True
+        GT -> False
+        LT -> go (succ power)
+      where
+        digitPowerSum = digitSum ^ power
+
+main = print $ digitPowerSums !! (target - 1) where
+    -- Numbers who are equal to their digit sum raised to some power
+    digitPowerSums = filter isDigitPower [1..]
---
+++ haskell/Euler120.hs
@@ -0,0 +1,12 @@
+module Euler120 where
+
+problemSquareRemainder :: Integral a => a -> a -> a
+problemSquareRemainder a n = numerator `mod` square
+  where
+    numerator = (a - 1) ^ n + (a + 1) ^ n
+    square = a ^ 2
+
+maxSquareRemainder :: Integral a => a -> a
+maxSquareRemainder a = maximum . map (problemSquareRemainder a) $ [1..a ^ 2 - 1]
+
+main = print . sum . map maxSquareRemainder $ [3..1000]
