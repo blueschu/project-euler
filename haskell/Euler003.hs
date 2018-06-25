7da6d02918b455e88aba709dc13673e5b9f9222d
Brian Schubert <brianm.schubert@gmail.com>
2018-06-25 13:28:41 -0400

    Improve readability of prime factoring

--- haskell/Euler003.hs
+++ haskell/Euler003.hs
@@ -3,14 +3,15 @@ module Euler003 where
 target = 600851475143
 
 primeFactors :: Integral a => a -> [a]
-primeFactors n = searchFactors 2 n
+primeFactors 0 = [1..]
+primeFactors n 
+  | n < 0     = primeFactors (-n)
+  | otherwise = searchFactors 2 n
   where
     searchFactors _ 1 = []
-    searchFactors _ 0 = [1..]
     searchFactors s n
       | n `mod` s == 0 = s:(searchFactors s (n `quot` s))
-      | n < 0 = searchFactors s (-n)
-      | otherwise = searchFactors (s + 1) n
+      | otherwise      = searchFactors (s + 1) n
 
 greatestPrimeFactor :: Integral a => a -> a
 greatestPrimeFactor = maximum . primeFactors
