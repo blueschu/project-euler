7acdad96ec3725ccde817005f999947b20b43b27
Brian Schubert <brianm.schubert@gmail.com>
2018-06-30 09:51:01 -0400

    Amend logical error in solution for problem 55

--- haskell/Euler055.hs
+++ haskell/Euler055.hs
@@ -11,9 +11,11 @@ checkDepth = 50
 reverseNum :: Integral a => a -> a
 reverseNum = fromIntegral . read . reverse . show . fromIntegral
 
+-- | Checks whether an integer is a Lychrel number.
 lychrel :: Integral a => Int -> a -> Bool
-lychrel depth = any (numericPalindrome 10) . take depth . drop 1 . iterate (\x -> x + reverseNum x)
+lychrel depth = all (not . numericPalindrome 10) . take depth . tail . iterate (\x -> x + reverseNum x)
 
+-- | List of numbers considred to be lychrels when checked to the given depth.
 lychrels :: Integral a => Int -> [a]
 lychrels depth = filter (lychrel depth) [1..]
 
