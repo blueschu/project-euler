5a4d4f858e12d65c8ba905caf9c4b9353f744144
Brian Schubert <brianm.schubert@gmail.com>
2018-07-02 13:25:10 -0400

    Amend error in problem 63

--- haskell/Euler063.hs
+++ haskell/Euler063.hs
@@ -7,7 +7,7 @@ nthPowersEqualToDigitCount :: Integral a => a -> [a]
 nthPowersEqualToDigitCount n = filter (hasMinDigits n) . takeWhile (hasMaxDigits n) . nthPowers $ n
   where
     hasMaxDigits count x = x < 10 ^ count
-    hasMinDigits count x = x > 10 ^ (count - 1)
+    hasMinDigits count x = x >= 10 ^ (count - 1)
 
 nDigitNumbersThatAreAnNthPower :: Integral a => [[a]]
 nDigitNumbersThatAreAnNthPower = takeWhile (not . null) . map nthPowersEqualToDigitCount $ [1..]
