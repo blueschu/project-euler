246c9de6a5564950f8cddb8e416783083c12c6d0
Brian Schubert <brianm.schubert@gmail.com>
2018-07-09 13:40:34 -0400

    Optimize python primality test

--- python/common.py
+++ python/common.py
@@ -7,36 +7,46 @@ import math
 from typing import Generator, List, Sequence, Set, Tuple
 
 
+_primes_cache = [2, 3]
+
+
 def is_prime(num: int) -> bool:
     """
     Returns True if num is a prime number, else False.
 
-    A simple, brute-force approach.
+    The primality of the given number is determined via trial by
+    division against every prime number that is less than or equal
+    to the sqaure root of the given number. This approach may be
+    impractical for numberswith large prime factors.
 
     >>> all(is_prime(i) for i in (2, 3, 5, 7, 11, 13, 163))
     True
     >>> any(is_prime(i) for i in (0, 1, 9, 10))
     False
     """
-    if num == 1:
+    if num in (0, 1):
         return False
-    if num == 2:
-        return True
 
-    top = int(math.ceil(math.sqrt(num))) + 1
-    return num % 2 != 0 and all(num % i != 0 for i in range(3, top, 2))
+    top = int(math.sqrt(num))
+
+    primes_below = itertools.takewhile(lambda x: x <= top, primes())
+
+    return all(num % p != 0 for p in primes_below)
 
 
 def primes() -> Generator[int, None, None]:
     """
     Yields every prime integer.
 
-
     >>> list(itertools.islice(primes(), 6))
     [2, 3, 5, 7, 11, 13]
     """
-    for i in itertools.count(2):
+    for p in _primes_cache:
+        yield p
+
+    for i in itertools.count(p + 2, 2):
         if is_prime(i):
+            _primes_cache.append(i)
             yield i
 
 
